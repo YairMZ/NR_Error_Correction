97751b133fe4f29da4afdc3c68c8d0bbc21025e9
Yair Mazal <yairmazal@gmail.com>
2021-09-03 14:53:14 +0300

    Fix issues of static type problems in FrameHeader class

--- protocol_meta/msg_header.py
+++ protocol_meta/msg_header.py
@@ -1,14 +1,17 @@
-from .protocol_meta import dialect_meta as meta
-import bitstring
+"""Handling of Mavlink headers"""
+from protocol_meta import dialect_meta as meta
+import bitstring  # type: ignore
 from utils.custom_exceptions import NonUint8
 from utils.bit_operations import hamming_distance
 
 
 class HeaderLength(Exception):
+    """Raised when a header of incorrect length is considered"""
     pass
 
 
 class NonExistentMsdId(Exception):
+    """Raised when trying to consider a msg id which doesn't exist in the protocol"""
     pass
 
 
@@ -30,11 +33,14 @@ def is_valid_header(buffer: bytes) -> bool:
 
 def hamming_distance_2_valid_header(buffer: bytes, max_len: int = None) -> tuple:
     """
-    The function calculates the Hamming distance to the closest valid header. A valid header starts with magic marker, and has valid msg_id and corresponding length
+    The function calculates the Hamming distance to the closest valid header. A valid header starts with magic marker,
+    and has valid msg_id and corresponding length
 
     :param buffer: buffer containing supposed header.
-    :param max_len: if the maximal length is apriori (such as for instance limited by buffer length), it can be sent to ignore messages too long. The length sent should be that of the payload.
-    :return: a tuple of (min_dist, chosen_msg_id) where chosen_msg_id the closest msg id with respect to valid headers, and min_dist is the minimal Hamming distance found.
+    :param max_len: if the maximal length is apriori (such as for instance limited by buffer length), it can be sent to
+    ignore messages too long. The length sent should be that of the payload.
+    :return: a tuple of (min_dist, chosen_msg_id) where chosen_msg_id the closest msg id with respect to valid headers,
+    and min_dist is the minimal Hamming distance found.
     """
     if len(buffer) != meta.header_len:
         raise HeaderLength("incorrect header length of {}".format(len(buffer)))
@@ -64,42 +70,50 @@ class FrameHeader:
     """A class holding a MAVLink message header"""
 
     def __init__(self, msg_id: int, sys_id: int, comp_id: int, seq: int):
-        if msg_id not in meta.msgs_length.keys():
+        if sys_id < 0 or comp_id < 0 or seq < 0 or sys_id > 255 or comp_id > 255 or seq > 255:
+            raise NonUint8("invalid input, msg_id: {}, comp_id: {}, seq: {}".format(msg_id, comp_id, seq))
+        if msg_id in meta.msgs_length.keys():
+            length: int = meta.msgs_length.get(msg_id)
+        else:
             if msg_id < 0 or msg_id > 255:
                 raise NonUint8(msg_id)
             else:
                 raise NonExistentMsdId("msg_id {} does not exist".format(msg_id))
-        if sys_id < 0 or comp_id < 0 or seq < 0 or sys_id > 255 or comp_id > 255 or seq > 255:
-            raise NonUint8("invalid input, msg_id: {}, comp_id: {}, seq: {}".format(msg_id, comp_id, seq))
-        length: int = meta.msgs_length.get(msg_id)
 
         self.buffer: bytes = bytes([meta.stx, length, seq, sys_id, comp_id, msg_id])
 
     @property
     def length(self) -> int:
+        """length of payload"""
         return self.buffer[1]
 
     @property
     def sequence(self) -> int:
+        """seq number of packet"""
         return self.buffer[2]
 
     @property
     def sys_id(self) -> int:
+        """sender sys id"""
         return self.buffer[3]
 
     @property
     def comp_id(self) -> int:
+        """sender comp id"""
         return self.buffer[4]
 
     @property
     def msg_id(self) -> int:
+        """msg if of msg"""
         return self.buffer[5]
 
     @property
     def bit_string(self) -> bitstring.Bits:
+        """bitstring representation of buffer"""
         return bitstring.Bits(bytes=self.buffer)
 
     def hamming_distance(self, some_buffer: bytes) -> int:
+        """Hamming distance between some_buffer and self.buffer"""
         if len(some_buffer) != meta.header_len:
             raise HeaderLength("incorrect header length of {}".format(len(some_buffer)))
         return hamming_distance(self.buffer, some_buffer)
