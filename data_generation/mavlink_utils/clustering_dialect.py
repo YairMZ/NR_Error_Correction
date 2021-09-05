4694bff64064b826f19bc46b4a4bf0e7f94e537c
Yair Mazal <yairmazal@gmail.com>
2021-09-05 03:58:21 +0300

    Edited Mavlink dialect manually. Copied crc code, as Github CI cannot install pymavlink for some reason.

--- data_generation/mavlink_utils/clustering_dialect.py
+++ data_generation/mavlink_utils/clustering_dialect.py
@@ -10,8 +10,41 @@ from builtins import range
 from builtins import object
 import struct, array, time, json, os, sys, platform
 
-from pymavlink.generator.mavcrc import x25crc
+# from pymavlink.generator.mavcrc import x25crc
 import hashlib
+
+
+class x25crc(object):
+    '''CRC-16/MCRF4XX - based on checksum.h from mavlink library'''
+    def __init__(self, buf=None):
+        self.crc = 0xffff
+        if buf is not None:
+            if isinstance(buf, str):
+                self.accumulate_str(buf)
+            else:
+                self.accumulate(buf)
+
+    def accumulate(self, buf):
+        '''add in some more bytes'''
+        accum = self.crc
+        for b in buf:
+            tmp = b ^ (accum & 0xff)
+            tmp = (tmp ^ (tmp<<4)) & 0xFF
+            accum = (accum>>8) ^ (tmp<<8) ^ (tmp<<3) ^ (tmp>>4)
+        self.crc = accum
+
+    def accumulate_str(self, buf):
+        '''add in some more bytes'''
+        accum = self.crc
+        import array
+        bytes_array = array.array('B')
+        try:  # if buf is bytes
+            bytes_array.frombytes(buf)
+        except TypeError:  # if buf is str
+            bytes_array.frombytes(buf.encode())
+        except AttributeError:  # Python < 3.2
+            bytes_array.fromstring(buf)
+        self.accumulate(bytes_array)
 
 WIRE_PROTOCOL_VERSION = '1.0'
 DIALECT = 'clustering_dialect'
