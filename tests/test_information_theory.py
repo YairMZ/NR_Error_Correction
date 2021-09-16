7526da75ceabeb6fb82449d620eb066e9d094ed3
Yair Mazal <yairmazal@gmail.com>
2021-09-16 11:44:32 +0300

    Separate probability calculation to two function, one returning alphabet, and one not. This is done for easier work typewise

--- tests/test_information_theory.py
+++ tests/test_information_theory.py
@@ -1,8 +1,9 @@
 """unit tests for the information theory module within the utils package"""
 import pytest
 import numpy as np
-from utils.information_theory import prob, entropy, typical_set_cardinality
+from utils.information_theory import prob, entropy, typical_set_cardinality, prob_with_alphabet
 from utils.custom_exceptions import UnsupportedDtype
+import numpy.typing as npt
 
 
 class TestProbability:
@@ -13,12 +14,12 @@ class TestProbability:
 
     def test_multi_dimension(self) -> None:
         data = np.random.randint(10, size=(10, 10))
-        p = prob(data)
+        p: npt.NDArray[np.float64] = prob(data)
         assert 10 == p.shape[0]
 
     def test_one_dimension(self) -> None:
         data = np.random.randint(10, size=10)
-        p = prob(data)
+        p: npt.NDArray[np.float64] = prob(data)
         assert 1 == p.ndim
 
     def test_probability(self) -> None:
@@ -38,42 +39,42 @@ class TestProbability:
     def test_gaps_in_alphabet_2d(self) -> None:
         # Data contain "missing" integers in characters caused broadcast exception in 2d data.
         data = np.array([[i for i in range(5, 11)], [i for i in range(15, 21)]])
-        p, alphabet = prob(data, return_labels=True)
+        p, alphabet = prob_with_alphabet(data)
         assert alphabet == data.flatten().tolist()
 
     def test_return_alphabet(self) -> None:
         data = np.array([[i for i in range(5, 11)], [i for i in range(15, 21)]])
-        p, alphabet = prob(data, return_labels=True)
+        p, alphabet = prob_with_alphabet(data)
         assert alphabet == data.flatten().tolist()
-        p, alphabet = prob(data[0], return_labels=True)
+        p, alphabet = prob_with_alphabet(data[0])
         assert alphabet == data[0].tolist()
 
 
 class TestEntropy:
     def test_1d_array(self) -> None:
-        p = np.ones(10)/10
+        p: npt.NDArray[np.int64] = np.ones(10)/10
         e = entropy(p)
         assert e == pytest.approx(np.log2(10))
-        assert isinstance(e, float)
+        assert isinstance(e, np.ndarray)
 
     def test_2d_array(self) -> None:
-        p = np.ones((2, 10))/10
-        e = entropy(p)
+        p: npt.NDArray[np.int64] = np.ones((2, 10))/10
+        e: npt.NDArray[np.float64] = entropy(p)
         assert e[0] == pytest.approx(np.log2(10))
         assert e[1] == pytest.approx(np.log2(10))
         assert isinstance(e, np.ndarray)
 
     def test_zero_mass_probability_1d(self) -> None:
-        p = np.ones(10) / 9
+        p: npt.NDArray[np.float64] = np.ones(10) / 9
         p[9] = 0
         e = entropy(p)
         assert e == pytest.approx(np.log2(9))
-        assert isinstance(e, float)
+        assert isinstance(e, np.ndarray)
 
     def test_zero_mass_probability_2d(self) -> None:
-        p = np.ones((2, 10))/9
+        p: npt.NDArray[np.float64] = np.ones((2, 10))/9
         p[:, 9] = 0
-        e = entropy(p)
+        e: npt.NDArray[np.float64] = entropy(p)
         assert e[0] == pytest.approx(np.log2(9))
         assert e[1] == pytest.approx(np.log2(9))
         assert isinstance(e, np.ndarray)
@@ -104,7 +105,3 @@ class TestCardinality:
         lb, ub = typical_set_cardinality(2, pk=np.array([1/2, 1/2]), eps=1e-15)
         assert lb == pytest.approx(4, abs=1e-5)
         assert ub == pytest.approx(4, abs=1e-5)
-
-    def test_bad_entropy_type(self) -> None:
-        with pytest.raises(ValueError):
-            typical_set_cardinality(2, ent=np.array(1))
--- utils/information_theory.py
+++ utils/information_theory.py
@@ -1,20 +1,19 @@
 """various statistical functions, information theory measures, and convenience wrappers around scipy"""
 import numpy as np
 from scipy.stats import entropy as scipy_ent  # type: ignore
-from typing import Union, Any, Optional
+from typing import Any, Optional
 from utils.custom_exceptions import UnsupportedDtype
 import numpy.typing as npt
 
 
-def prob(data: npt.NDArray[Any], return_labels: bool = False
-         ) -> Union[npt.NDArray[np.float64], tuple[npt.NDArray[np.float64], list[Any]]]:
+def prob_with_alphabet(data: npt.NDArray[Any]) -> tuple[npt.NDArray[np.float64], list[Any]]:
     """Estimate the probability distribution given samples. Doesn't work with floating point values.
 
     :param data: a 1D or 2D numpy array. Each row is a dimension and each column an observation
-    :param return_labels: if true, labels of frequencies are also returned. Tuple is returned instead of array.
-    :return: a 2D array of of probabilities. Rows are RV dimension, and columns are alphabet size
+    :return: a tuple: prob, alphabet. prob is a 2D array of of probabilities. Rows are RV dimension, and columns
+    are alphabet size, alphabet is a list with the inferred alphabet.
     """
-    if data.dtype in [float, np.float64]:
+    if data.dtype in [float, np.dtype(np.float64)]:
         raise UnsupportedDtype()
     # TODO - add ability to return joint distribution - can be done with np.unique along axis
 
@@ -28,34 +27,40 @@ def prob(data: npt.NDArray[Any], return_labels: bool = False
             for val, count in zip(values, counts):
                 pr[row_idx][alphabet.index(val)] = count
         pr = np.divide(pr, num_samples)
-        if return_labels:
-            return pr, alphabet
-        else:
-            return pr
+        return pr, alphabet
     elif data.ndim == 1:
         num_samples = data.shape[0]
         alphabet_arr, pr = np.unique(data, return_counts=True)
         pr = np.divide(pr, num_samples)
-        if return_labels:
-            return pr, alphabet_arr.tolist()
-        else:
-            return pr
+        return pr, alphabet_arr.tolist()
     else:
         raise ValueError("only scalar and vector RV's currently supported (dim=1,2)")
 
 
-def entropy(pk: npt.NDArray[Any], base: int = 2) -> Union[npt.NDArray[Any], float]:
+def prob(data: npt.NDArray[Any]) -> npt.NDArray[np.float64]:
+    """A wrapper around prob_with_alphabet which doesn't return the alphabet.
+
+    :param data: a 1D or 2D numpy array. Each row is a dimension and each column an observation
+    :return: a 2D array of of probabilities. Rows are RV dimension, and columns are alphabet size.
+    """
+    pr, alphabet = prob_with_alphabet(data)
+    return pr
+
+
+def entropy(pk: npt.NDArray[Any], base: int = 2) -> npt.NDArray[Any]:
     """This is a wrapper around scipy's entropy function. Seamlessly treat 1d and 2d arrays.
 
     :param pk: an array representing a PMF. number of columns is alphabet size, and number of rows is number of RV's.
     :param base: base of logarithm for entropy calculation. Defaults to 2 (entropy in units of bits).
-    :rtype: Union[npt.NDArray[Any], float]
+    :rtype: np.ndarray
     """
+    if pk.ndim not in (1, 2):
+        raise ValueError("only scalar and vector RV's currently supported (dim=1,2)")
     if pk.ndim == 2:
-        return scipy_ent(pk, base=base, axis=1)
-    if pk.ndim == 1:
-        return scipy_ent(pk, base=base)
-    raise ValueError("only scalar and vector RV's currently supported (dim=1,2)")
+        ent = scipy_ent(pk, base=base, axis=1)
+    else:
+        ent = scipy_ent(pk, base=base)
+    return ent if isinstance(ent, np.ndarray) else np.array(ent)
 
 
 def typical_set_cardinality(n: int, pk: Optional[npt.NDArray[Any]] = None, ent: Optional[float] = None,
@@ -67,7 +72,8 @@ def typical_set_cardinality(n: int, pk: Optional[npt.NDArray[Any]] = None, ent: 
     https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-441-information-theory-spring-2010/lecture-notes/MIT6_441S10_lec03.pdf
     :param n: The length of the sequence
     :param pk: Defaults to None. If specified, a 1d array representing a PMF of a univariate RV.
-    :param ent: The entropy of the variable. Defaults to None, in which case it is inferred from distribution. If specified, it is used and pk is ignored
+    :param ent: The entropy of the variable. Defaults to None, in which case it is inferred from distribution.
+    If specified, it is used and pk is ignored
     :param eps: The epsilon used for the calculation
     :return: An tuple representing a lower and  upper bounds on the cardinality of the typical set of sequences of length n.
     """
@@ -77,11 +83,9 @@ def typical_set_cardinality(n: int, pk: Optional[npt.NDArray[Any]] = None, ent: 
         if pk is None:
             raise ValueError()
         else:
-            ent_f = entropy(pk, base=2)
+            ent_f = float(entropy(pk, base=2))
     else:
         ent_f = ent
-    if not isinstance(ent_f, (float, int)):
-        raise ValueError()
     ub = 2**(n * (ent_f + eps))
     lb = (1 - eps) * 2**(n * (ent_f - eps))
     return lb, ub
