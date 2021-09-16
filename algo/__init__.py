a31579352c0f1f9738ee10807440a13946a4b7c0
Yair M <yairmazal@gmail.com>
2021-09-16 13:35:06 +0300

    Mypy work
    * Add more tests to mypy config file
    
    * explicit export of modules
    
    * Separate probability calculation to two function, one returning alphabet, and one not.
    This is done for easier work typewise

--- algo/__init__.py
+++ algo/__init__.py
@@ -1,3 +1,4 @@
 """Package for creating algorithms for error correction"""
 from .correction_algorithm import CorrectionAlgorithm, AlgorithmType
 from .entropy_algorithm import EntropyAlgorithm
+__all__: list[str] = ["CorrectionAlgorithm", "AlgorithmType", "EntropyAlgorithm"]
--- protocol_meta/__init__.py
+++ protocol_meta/__init__.py
@@ -2,3 +2,5 @@
 from .protocol_meta import MAVError
 from .protocol_meta import dialect_meta as dialect_meta
 from .msg_header import hamming_distance_2_valid_header, FrameHeader, is_valid_header, NonExistentMsdId
+__all__: list[str] = ["MAVError", "dialect_meta", "hamming_distance_2_valid_header", "FrameHeader",
+                      "is_valid_header", "NonExistentMsdId"]
